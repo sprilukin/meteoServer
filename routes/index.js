4bb604a372365f4d898e5442aa4ffcbd9a77ff1d
Sergey Prilukin <sprilukin@gmail.com>
2015-12-29 00:11:03 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+/* GET Userlist page. */
+router.get('/meteo', function(req, res) {
+    var db = req.db;
+    var collection = db.get('bl999SensorCollection');
+    collection.find({}, {}, function(e, docs) {
+        res.render('bl999SensorList', {
+            "sensorInfoCollection": docs
+        });
+    });
+});
+
+router.get('/meteo/json', function(req, res) {
+    var db = req.db;
+    var collection = db.get('bl999SensorCollection');
+    collection.find({}, {}, function(e, docs) {
+        res.setHeader('content-type', 'application/json');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.send(docs);
+    });
+});
+
+/* POST to Add User Service */
+router.post('/meteo', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var channel = req.body.channel;
+    var powerUUID = req.body.powerUUID;
+    var battery = req.body.battery;
+    var temperature = req.body.temperature;
+    var humidity = req.body.humidity;
+
+    console.log("headers: ", req.headers);
+    console.log("channel: ", channel);
+    console.log("powerUUID: ", powerUUID);
+    console.log("battery: ", battery);
+    console.log("temperature: ", temperature);
+    console.log("humidity: ", humidity);
+
+    // Set our collection
+    var collection = db.get('bl999SensorCollection');
+
+    // Submit to the DB
+    collection.insert({
+        "channel": Number(channel),
+        "powerUUID": Number(powerUUID),
+        "battery": Number(battery),
+        "temperature": Number(temperature),
+        "humidity": Number(humidity),
+        "date": new Date()
+    }, function(err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send(500, "There was a problem adding the information to the database.");
+        } else {
+            // And forward to success page
+            res.send("Created");
+        }
+    });
+});
+
+module.exports = router;
